034529fac90abf5213c36b96e1765c04c16e3382
Gonzalo Villegas <gonzalo.villegas@quind.io>
2026-04-09 09:40:18 -0500

    fix1-test3

--- src/EntryPoints/Api/modules/carga-masiva/domain/ports/file-parser.service.ts
+++ src/EntryPoints/Api/modules/carga-masiva/domain/ports/file-parser.service.ts
@@ -4,4 +4,6 @@ export const IFileParserServiceToken = "IFileParserService";
 
 export interface IFileParserService {
     parse(fileContent: string): Promise<PackageDataDto[]>;
+
+    parseCsv(fileContent: string): Promise<PackageDataDto[]>;
 }
